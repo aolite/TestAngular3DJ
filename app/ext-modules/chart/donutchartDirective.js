9e9f7ac6b942e8e60c4593db73382b33881bc7fd
aolite <aitorcorchero@gmail.com>
2016-06-23 10:09:41 +0200

    Add a toolTip to the Donut Graph.

--- app/ext-modules/chart/donutchartDirective.js
+++ app/ext-modules/chart/donutchartDirective.js
@@ -25,6 +25,7 @@ angular.module('chartModule')
         var svg = d3.select(element[0]).append('svg')
         var pie = d3.layout.pie().sort(null);
 
+
         var arc = d3.svg.arc()
           .outerRadius(min / 2 * 0.9)
           .innerRadius(min / 2 * 0.5);
@@ -40,6 +41,10 @@ angular.module('chartModule')
 
         var arcs = g.selectAll('path');
 
+        var div = d3.select("body").append("div")
+          .attr("class", "tooltip")
+          .style("opacity", 0);
+
         scope.$watch('data', function (data){
           if(!data){ return; }
           arcs = arcs.data(pie(data));
@@ -47,7 +52,23 @@ angular.module('chartModule')
           arcs.enter().append('path')
             .style('stroke', 'white')
             .attr('d', arc)
-            .attr('fill', function(d, i){ return color(i) });
+            .attr('fill', function(d, i){ return color(i) })
+            .on('mouseover', function(d){
+              scope.$apply(function() {
+                //console.log('Mouse Event '+ d.data);
+                div.transition()
+                  .duration(200)
+                  .style("opacity", .9);
+                div .html(Math.round(d.data))
+                  .style("left", (d3.event.pageX) + "px")
+                  .style("top", (d3.event.pageY - 28) + "px");
+              });
+            })
+            .on("mouseout", function(d) {
+              div.transition()
+                .duration(500)
+                .style("opacity", 0);
+            });
 
           arcs.data(pie(data)).attr('d', arc);
         }, true);
