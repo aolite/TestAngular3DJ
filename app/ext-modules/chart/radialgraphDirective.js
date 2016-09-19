7d4ebd5cac830f182b37d73d62ce16cfcba6aaf5
aolite <aitorcorchero@gmail.com>
2016-09-19 08:43:04 +0200

    grafico de radio inicial

--- app/ext-modules/chart/radialgraphDirective.js
+++ app/ext-modules/chart/radialgraphDirective.js
@@ -27,6 +27,51 @@ angular.module('app')
             'category': "Saving"
           }
         };
+
+        var orderedIdsTable = ['electricity', 'light', 'objective', 'pv', 'tv'],
+          idsInfoTable = {
+            'light': {
+              'id': 'light',
+              'nombre': 'light',
+              'nombreAbrev': 'light',
+              'color': '7EAADD',
+              'highlightColor': 'c6d1dd',
+              'icon': '\\e82b',
+            },
+            'electricity': {
+              'id': 'hid',
+              'nombre': 'electricity',
+              'nombreAbrev': 'electricity',
+              'color': '33537A',
+              'highlightColor': '446fa4',
+              'icon': '\\e82d'
+            },
+            'tv': {
+              'id': 'sol',
+              'nombre': 'tv',
+              'nombreAbrev': 'tv',
+              'color': 'F5A623',
+              'highlightColor': 'f5cc89',
+              'icon': '\\e82c'
+            },
+            'pv': {
+              'id': 'pv',
+              'nombre': 'pv',
+              'nombreAbrev': 'pv',
+              'color': '9B9B9B',
+              'highlightColor': 'bdbdbd',
+              'icon': '\\e800'
+            },
+            'objective': {
+              'id': 'objective',
+              'nombre': 'objective',
+              'nombreAbrev': 'objective',
+              'color': '6F93A4',
+              'highlightColor': '96C6DD',
+              'icon': '\\e806'
+            }
+          }
+
         var energyConsumptionData = {
 
           'light':{
@@ -67,6 +112,7 @@ angular.module('app')
 
         //Create the graph place
 
+        var arcPart = (360 / jsonData.length) / 1.05;
         var canvasWidth = 960;
         var canvasHeight = 600;
         var radix = 250;
@@ -77,10 +123,17 @@ angular.module('app')
         var rotationHours=0;
         var Radix2Bar= (radix *.75)*2;
         var isoDateFormat = d3.time.format.utc("%Y-%m-%dT%H:%M:%S.%LZ");
+        var scaleRadix = d3.scale.linear().range([0, radix]);
+
+        var rotHour = d3.scale.linear()
+          .domain([0, 24 * 60])
+          .range([0, 360]);
 
         var min = Math.min(canvasWidth, canvasHeight);
         var svg = d3.select(element[0]).append('svg')
           .attr({width: canvasWidth, height: canvasHeight});
+
+
 
         //Create a rectangle for place the graph
         svg.append('rect')
@@ -180,6 +233,31 @@ angular.module('app')
 
         // Print the consumption values for each of the hours.
 
+        /*
+         <g class="rad" id="id-21:20-dia-15" opacity="0.4" transform="translate(384,300)">
+            <path fill="#7EAADD" d="M-34.574056563273665,-41.2037561198368A53.787683586704446,53.787683586704446 0 0,1 -32.832460432141175,-42.60451205676834L0,0Z"></path>
+            <path fill="#33537A" d="M-55.07005416665147,-65.6299348974646A85.6737954135532,85.6737954135532 0 0,1 -52.2960148200572,-67.86107908443341L-32.832460432141175,
+                  -42.60451205676834A53.787683586704446,53.787683586704446 0 0,0 -34.574056563273665,-41.2037561198368Z"></path>
+            <path fill="#F5A623" d="M-56.29670856867181,-67.09180468794385A87.58213089409945,87.58213089409945 0 0,1 -53.46087905994006,-69.37264635351337L-52.2960148200572,
+                  -67.86107908443341A85.6737954135532,85.6737954135532 0 0,0 -55.07005416665147,-65.6299348974646Z"></path>
+            <path fill="#9B9B9B" d="M-77.31545762607516,-92.14097438894044A120.28149961350167,120.28149961350167 0 0,1 -73.42085238552768,-95.27338339880323L-53.46087905994006,
+                  -69.37264635351337A87.58213089409945,87.58213089409945 0 0,0 -56.29670856867181,-67.09180468794385Z"></path>
+            <path fill="#6F93A4" d="M-77.31545762607516,-92.14097438894044A120.28149961350167,120.28149961350167 0 0,1 -73.42085238552768,-95.27338339880323L-73.42085238552768,
+                  -95.27338339880323A120.28149961350167,120.28149961350167 0 0,0 -77.31545762607516,-92.14097438894044Z"></path>
+            <path fill="#BD10E0" d="M-113.60268974660087,-135.38641363387694A176.734411234218,176.734411234218 0 0,1 -107.88019072231539,-139.98898729551027L-73.42085238552768,
+                  -95.27338339880323A120.28149961350167,120.28149961350167 0 0,0 -77.31545762607516,-92.14097438894044Z"></path>
+            <path fill="#583636" d="M-143.8860437812886,-171.47670960051843A223.8469466632311,223.8469466632311 0 0,1 -136.63808383436,-177.30620287098205L-107.88019072231539,
+                  -139.98898729551027A176.734411234218,176.734411234218 0 0,0 -113.60268974660087,-135.38641363387694Z"></path>
+            <path fill="#3D4163" d="M-160.69690242163486,-191.51111077974446A249.99999999999997,249.99999999999997 0 0,1 -152.6021304636406,-198.02169017044068L-136.63808383436,
+                  -177.30620287098205A223.8469466632311,223.8469466632311 0 0,0 -143.8860437812886,-171.47670960051843Z"></path>
+          </g>
+         */
+
+
+        var maxData = d3.max(jsonData, function (d){return d.demand});
+        console.log (maxData);
+        scaleRadix.domain ([0, maxData]);
+
         var gConsumptionValues = svg.append('g').attr('id', 'consumptionBars');
 
         //Draw the mouse as a circle
@@ -218,12 +296,124 @@ angular.module('app')
           .attr ('id', function (d){
             var tStamp = isoDateFormat.parse(d.timeStamp);
             return ['id-' , tStamp.getHours() , ':' , tStamp.getMinutes() , '-dia-' , tStamp.getDate()].join("");
-          });
+          })
+          .each (function (d){
+            var group = d3.select(this);
 
-        consumptionBars.each(function(d,i){
+
+            group.selectAll('path')
+              .data(orderedIdsTable)
+              .enter().append('path')
+              .on('click', function() {
+                var that = d3.select(this);
+                console.log("click", isoDateFormat.parse(d.timeStamp), that.datum(), d[that.datum()])
+              })
+              .on('mouseover', function() {
+                var that = d3.select(this);
+                that
+                  .attr('fill', '#' + idsInfoTable[that.datum()].highlightColor);
+              })
+              .on('mouseout', function() {
+                var that = d3.select(this);
+                that
+                  .attr('fill', '#' + idsInfoTable[that.datum()].color);
+              })
+              .attr('fill', function(d, n) {
+                var that = d3.select(this);
+                return '#' + idsInfoTable[that.datum()].color;
+              });
+
+            group.selectAll('path');
+
+            //define a variable that aggregattes all the energy consumption values
+            var consumptionAgg = [d.light, d.electricity, d.tv, d.pv];
+            var demandEnergy = normaliseConsumption (consumptionAgg);
+
+            console.log("demandEnergy", demandEnergy);
+            var gConsumptionBar,
+              n=0,
+              accInner = 0;
+
+            var arc = d3.svg.arc(),
+              tsDate = isoDateFormat.parse(d.timeStamp),
+              h = tsDate.getHours(),
+              m = tsDate.getMinutes(),
+              angle = rotHour((h * 60) + m),
+              path;
+
+            paths.each (function (){
+
+
+              gConsumptionBar = (demandEnergy[n]/100) * scaleRadix (d.demand);
+
+              console.log ("trace gConsumption bar", gConsumptionBar,n);
+
+              d3.select(this).attr ('d', arc.startAngle(function (){
+                return grad2rad(angle);})
+                .endAngle (function (){
+                  return grad2rad (angle + arcPart)
+                }).outerRadius (function (){
+                  return gConsumptionBar + accInner
+                }).innerRadius (function (){
+                  return accInner;
+                }));
+
+              accInner += gConsumptionBar;
+
+              n++;
+
+
+            });
+
+
+            n++;
+          }).attr('opacity', 0.4)
+            .attr('transform', 'translate(' + centerX + ',' + centerY + ')');
+
+        /*consumptionBars.each(function(d){
           var paths = d3.select(this).selectAll('path');
 
-        });
+          //define a variable that aggregattes all the energy consumption values
+          var consumptionAgg = [d.light, d.electricity, d.tv, d.pv];
+          var demandEnergy = normaliseConsumption (consumptionAgg);
+
+          console.log("demandEnergy", demandEnergy);
+          var gConsumptionBar,
+            n=0,
+            accInner = 0;
+
+          var arc = d3.svg.arc(),
+            tsDate = isoDateFormat.parse(d.timeStamp),
+            h = tsDate.getHours(),
+            m = tsDate.getMinutes(),
+            angle = rotHour((h * 60) + m),
+            path;
+
+          paths.each (function (){
+
+
+            gConsumptionBar = (demandEnergy[n]/100) * scaleRadix (d.demand);
+
+            console.log ("trace gConsumption bar", gConsumptionBar,n);
+
+            d3.select(this).attr ('d', arc.startAngle(function (){
+              return grad2rad(angle);})
+              .endAngle (function (){
+                return grad2rad (angle + arcPart)
+              }).outerRadius (function (){
+                return gConsumptionBar + accInner
+              }).innerRadius (function (){
+                return accInner;
+              }));
+
+            accInner += gConsumptionBar;
+
+            n++;
+
+
+          });
+        });*/
+
 
       }
     };
@@ -241,6 +431,7 @@ angular.module('app')
           "electricity": 0,
           "tv": 0,
           "pv":0,
+          "demand:0"
           "timeStamp": todayDate.toISOString()
         }*/
       ];
@@ -248,10 +439,11 @@ angular.module('app')
       var obj = {};
 
       for (var i=0; i< 24*6; i++){
-        obj.light= Math.random()*100;
-        obj.electricity= Math.random()*100;
-        obj.tv= Math.random()*100;
-        obj.pv= Math.random()*100;
+        obj.light= Math.random()*2000;
+        obj.electricity= Math.random()*2000;
+        obj.tv= Math.random()*300;
+        obj.pv= Math.random()*1000;
+        obj.demand= obj.light+obj.electricity+obj.tv+obj.pv;
         obj.objective= Math.random()*100;
 
         obj.timeStamp= todayDate.toISOString();
@@ -307,6 +499,20 @@ angular.module('app')
       return energyConsumptionData;
     }
 
+    function normaliseConsumption(arr){
+
+      var dataNormalization,
+        normParameter;
+
+      normParameter = arr.reduce(function (a,b){return a+b},0);
+
+      dataNormalization= arr.map (function (a){return a*100/normParameter});
+
+
+      return dataNormalization;
+
+    }
+
     function grades2Rad (grades){
       return (Math.PI/180)*grades;
     }
@@ -316,6 +522,10 @@ angular.module('app')
       var med = (measure)? measure: 100;
 
       return (data*med)/mx;
+    }
+
+    function grad2rad (grad){
+      return Math.PI / 180 * grad;
     }
 
 
